ce77d59be451c752416d66318e3526bf51ec68d3
Halohusaini <hallohusaini56@gmail.com>
2024-03-30 22:20:06 +0100

    Update Bookcard

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function BookCard({ book }) {
-    const { key, title, author_name, first_publish_year, cover_i } = book;
+    const { key, title, author_name, first_publish_year, cover_i, amazon_id } = book;
     const coverUrl = cover_i ? `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg` : 'https://via.placeholder.com/150'
 
-    const amazonSearchUrl = amazon_id ? `https://www.amazon.com/s?k=${amazon_id}` : null;
+    const amazonSearchUrl = `https://www.amazon.com/s?k=0395082544${amazon_id}`;
     
     return (
         <div>
@@ -14,7 +14,7 @@ export default function BookCard({ book }) {
             <p>Author: {author_name?.[0]}</p>
             <p>First published in: {first_publish_year}</p>
             {amazonSearchUrl && (
-                <a href={amazonSearchUrl} target="_blank" rel="noopener noreferrer">Se på Amazon</a>
+                <button onClick={() => window.open(amazonSearchUrl, "_blank")}>Se på Amazon</button>
             )}
         </div>
     );
